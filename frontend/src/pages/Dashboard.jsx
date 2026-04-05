ed645910ee48bf0b22b3fad7a5c199082d9e447c
Jeevansh Grover <groverjeevansh0243@gmail.com>
2026-04-06 03:20:00 +0530

    implement Dashboard component with income, expenses, and balance summary cards; add trends and pie charts for visualization

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import SummaryCard from '../components/dashboard/SummaryCard.jsx'
+import {
+  selectTotalIncome,
+  selectTotalExpenses,
+  selectBalance
+} from '../store/selectors.js'
+import TrendsChart from '../charts/TrendsChart.jsx'
+import PieChart from '../charts/ExpensePieChart.jsx'
+
 
 function Dashboard() {
+  const income = useSelector(selectTotalIncome);
+  const expenses = useSelector(selectTotalExpenses);
+  const balance = useSelector(selectBalance);
+
   return (
-    <div>Dashboard</div>
-  )
+    <div className="space-y-6">
+
+      <div className="flex flex-col sm:flex-row gap-4">
+        <SummaryCard title="Total Income" value={income} type="income" />
+        <SummaryCard title="Total Expenses" value={expenses} type="expense" />
+        <SummaryCard title="Net Balance" value={balance} type="balance" />
+      </div>
+
+      <div className="bg-white rounded-xl shadow p-4 space-y-3">
+        <div>
+          <h1>Cash flow Trend</h1>
+          <span className='text-sm text-gray-500'>Last 8 months</span>
+        </div>
+          
+        <TrendsChart/>
+  
+      </div>
+
+      <div className="bg-white rounded-xl shadow p-4">
+        <PieChart/>
+      </div>
+
+    </div>
+  );
 }
 
 export default Dashboard
